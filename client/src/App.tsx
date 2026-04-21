0ce5fc88661b4a78078a2a07fe1590932e78f0be
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-04-21 14:37:25 +0200

    Syncronized Main and Sidebar to populate the cards with the selected set from the Sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
+import { useSelectedSet } from './hooks/useSelectedSet';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Main from './components/Main';
 
 function App() {
+    const { selectedSet, setSelectedSet } = useSelectedSet();
+
     return (
         <div className="flex flex-col h-dvh">
             <Header />
             <div className="flex flex-1 min-h-0">
-                <Sidebar />
-                <Main />
+                <Sidebar onSetSelect={setSelectedSet} selectedSet={selectedSet} />
+                <Main selectedSet={selectedSet} />
             </div>
         </div>
     )
 }
 
-export default App
+export default App;
